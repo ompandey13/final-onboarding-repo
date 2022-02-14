fb13a4b7d04f6b6318017971e3a89fb2060f0034
om pandey <om.pandey@aircall.io>
2022-02-14 11:32:06 +0530

    middleware

--- packages/server/src/post.ts
+++ packages/server/src/post.ts
@@ -1,16 +1,48 @@
+import { classValidator } from "@lambda-middleware/class-validator";
+import { composeHandler } from "@lambda-middleware/compose";
+import { errorHandler } from "@lambda-middleware/http-error-handler";
 import * as AWS from "aws-sdk";
+import { IsString } from "class-validator";
 AWS.config.update({ region: "us-east-1" });
 
 const dynamodb = new AWS.DynamoDB.DocumentClient();
 
 const tableName = process.env.TABLE_NAME || "demo";
 
-exports.handler = async (event: {
-  pathParameters: { userid: string };
-  body: string;
-}) => {
-  let userid = event.pathParameters?.userid;
-  let { firstName, lastName, email, website } = JSON.parse(event.body);
+// Define a validator for the body via class-validator
+class NameBody {
+  constructor(
+    firstName: string,
+    lastName: string,
+    email: string,
+    website: string,
+    userid: string
+  ) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.email = email;
+    this.website = website;
+    this.userid = userid;
+  }
+
+  @IsString()
+  public firstName: string;
+
+  @IsString()
+  public lastName: string;
+
+  @IsString()
+  public email: string;
+
+  @IsString()
+  public website: string;
+
+  @IsString()
+  public userid: string;
+}
+
+async function addUser(event: { body: NameBody }) {
+  let { firstName, lastName, email, website, userid } = event.body;
   let item = {
     userid: userid,
     firstName: firstName,
@@ -32,4 +64,14 @@ exports.handler = async (event: {
       message: "Data inserted/updated successfully",
     }),
   };
-};
+}
+
+exports.handler = composeHandler(
+  errorHandler(),
+  classValidator({
+    bodyType: NameBody,
+    transformer: {},
+    validator: { skipMissingProperties: true },
+  }),
+  addUser
+);
